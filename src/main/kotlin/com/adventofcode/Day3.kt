d1c7ea7bb32cf5eae0a18a7f33e207ddbb2d1ed3
Kurt Vooys <kurt.vooys@gmail.com>
2021-12-03 18:19:32 +0100

    Day 3!

---
+++ src/main/kotlin/com/adventofcode/Day3.kt
@@ -0,0 +1,114 @@
+package com.adventofcode
+
+import java.io.File
+import kotlin.math.pow
+
+fun toDecimal(binaryNumber : String) : Int {
+    var sum = 0
+    binaryNumber.reversed().forEachIndexed {
+            k, v -> sum += v.toString().toInt() * 2.0.pow(k.toDouble()).toInt()
+    }
+    return sum
+}
+
+
+fun findMostAndLeastCommonBits(input: List<String>): Int {
+    var mostCommonResult = ""
+    var leastCommonResult = ""
+    for (i in 0 until input[0].length) {
+        var zeroes = 0
+        var ones = 0
+
+        for (inp in input) {
+            if (inp[i] == '0'){
+                zeroes += 1
+            }
+            else {
+                ones += 1
+            }
+        }
+        if (zeroes > ones) {
+            mostCommonResult += '0'
+            leastCommonResult += '1'
+        }
+        else {
+            mostCommonResult += '1'
+            leastCommonResult += '0'
+        }
+
+    }
+    return toDecimal(mostCommonResult) * toDecimal(leastCommonResult)
+}
+
+fun selectSubsetOfDigits(input: List<String>, index:Int, type: String) : List<String> {
+    var zeroes = 0
+    var ones = 0
+    for (inp in input) {
+        if (inp[index] == '0'){
+            zeroes += 1
+        }
+        else {
+            ones += 1
+        }
+    }
+    if (type == "most"){
+        if (zeroes > ones) {
+            return input.filter { i -> i[index] == '0' }
+        }
+        else {
+            return input.filter { i -> i[index] == '1' }
+        }
+    }
+    else {
+        if (zeroes <= ones) {
+            return input.filter { i -> i[index] == '0' }
+        }
+        else {
+            return input.filter { i -> i[index] == '1' }
+        }
+
+    }
+
+}
+
+
+fun keepMostCommonDigitOrOnes(input: List<String>) : Int {
+    var strLength = input[0].length
+    var index = strLength-1
+    var current_input = input;
+    for (i in 0 until strLength) {
+        current_input = selectSubsetOfDigits(current_input, i, "most")
+        if (current_input.size == 1){
+            return toDecimal(current_input[0])
+        }
+        index--
+        println(current_input)
+        println(current_input.size)
+    }
+    return toDecimal(current_input[0])
+}
+
+fun keepLeastCommonDigitsOrZeroes(input: List<String>) : Int {
+    var strLength = input[0].length
+    var index = strLength-1
+    var current_input = input;
+    for (i in 0 until strLength) {
+        current_input = selectSubsetOfDigits(current_input, i, "least")
+        if (current_input.size == 1){
+            return toDecimal(current_input[0])
+        }
+        index--
+    }
+    return toDecimal(current_input[0])
+}
+
+
+fun day3Results(rootPath: String) {
+    var testInput: List<String> = File(rootPath + "day3_test_input.txt").useLines { it.toList() }
+    var input: List<String> = File(rootPath + "day3_input.txt").useLines { it.toList() }
+    println(findMostAndLeastCommonBits(testInput))
+    println(findMostAndLeastCommonBits(input))
+    println(keepMostCommonDigitOrOnes(testInput) * keepLeastCommonDigitsOrZeroes(testInput))
+    println(keepMostCommonDigitOrOnes(input) * keepLeastCommonDigitsOrZeroes(input))
+
+}
--- src/main/kotlin/com/adventofcode/Main.kt
+++ src/main/kotlin/com/adventofcode/Main.kt
@@ -4,5 +4,6 @@ fun main() {
     var rootPath = "C:\\Users\\kvooy\\Documents\\Code Projects\\AdventOfCode2021\\src\\main\\resources\\"
 
 //    day1Results(rootPath)
-    day2Results(rootPath)
+//    day2Results(rootPath)
+    day3Results(rootPath)
 }
